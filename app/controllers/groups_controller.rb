54e337c3eb375359783004364111082d6fced18d
linfgit <linfgg1@gmail.com>
2017-09-13 18:22:01 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,27 +17,29 @@ class GroupsController < ApplicationController
 
   def create
    @group = Group.new(group_params)
-   @group.save
-
+   if @group.save
      redirect_to groups_path
- end
+   else
+     render :new
+   end
+  end
 
- def update
-   @group = Group.find(params[:id])
-   @group.update(group_params)
-   redirect_to groups_path, notice: "Update Success"
- end
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
+  end
 
- def destroy
-   @group = Gruop.find(params[:id])
-   @group.destroy
-   flash[:alert] = "Group deleted"
-   redirect_to groups_path 
- end
+  def destroy
+     @group = Gruop.find(params[:id])
+     @group.destroy
+     flash[:alert] = "Group deleted"
+     redirect_to groups_path
+  end
 
- private
+private
 
-def group_params
-  params.require(:group).permit(:title, :description)
- end
+  def group_params
+   params.require(:group).permit(:title, :description)
+  end
 end
